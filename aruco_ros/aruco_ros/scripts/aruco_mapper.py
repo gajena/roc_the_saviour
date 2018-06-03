d112cecd40800e3ff45b3955e37e805d1d049736
Ashwin Kuruttukulam <ashwinvk94@gmail.com>
2018-06-03 05:28:07 -0500

    Add cam wrt tag transformation

--- aruco_ros/aruco_ros/scripts/aruco_mapper.py
+++ aruco_ros/aruco_ros/scripts/aruco_mapper.py
@@ -30,12 +30,14 @@ class test:
 		self.tag_2_detected_flag = False
 		self.tag_3_detected_flag = False
 		self.tag_4_detected_flag = False
+		self.tag_2_diff_calculated_flag = False
+		self.tag_1_pos_calculated_flag = False
 
 		startup_start = timeit.default_timer()
 		print('start')
 
 		#Rate init
-		self.rate = rospy.Rate(20.0) # MUST be more then 2Hz
+		self.rate = rospy.Rate(200.0) # MUST be more then 2Hz
 
 		#Rate init
 		self.rate_tag_detector = rospy.Rate(200.0) # MUST be more then 2Hz
@@ -46,67 +48,122 @@ class test:
 		#Local setpoint publisher init
 		#self.setpoint_raw_pub = rospy.Publisher("/mavros/setpoint_raw/attitude", AttitudeTarget, queue_size=10)
 
-		self.cam_wrt_tag_1_pub = rospy.Publisher("/aruco_mapper/cam_pose", PoseStamped, queue_size=10)
+		self.cam_wrt_tag_1_pub = rospy.Publisher("/aruco_mapper/unfiltered_cam_pose", PoseStamped, queue_size=10)
 
 		#April tag pose subscriber init code
 		self.aruco_tag_subscriber1 = rospy.Subscriber("/simple_n/pose",Pose, self.callback_tag_detection1,  queue_size = 1)
 		#April tag pose subscriber init code
 		self.aruco_tag_subscriber2 = rospy.Subscriber("/simple_n/pose2",Pose, self.callback_tag_detection2,  queue_size = 1)
-		#April tag pose subscriber init code
-		self.aruco_tag_subscriber3 = rospy.Subscriber("/simple_n/pose3",Pose, self.callback_tag_detection3,  queue_size = 1)
-		#April tag pose subscriber init code
-		self.aruco_tag_subscriber4 = rospy.Subscriber("/simple_n/pose4",Pose, self.callback_tag_detection4,  queue_size = 1)
-
+		
 		#Target pose publisher using aruco pose
 		while not rospy.is_shutdown():
+			if self.tag_2_detected_flag==True and self.tag_1_pos_calculated_flag==True:
+				print('\n')
 
-			if self.tag_2_detected_flag == True:
-				print('Publishing using tag 2')
+				print('USING TAG 2:')
+				print('Position of cam wrt tag 2:')
+				print(self.cam_tag_2_pos_x)
+				print(self.cam_tag_2_pos_y)
+				print(self.cam_tag_2_pos_z)
+				
+				print('\n')
+				print('Position of tag2 wrt tag 1:')
+				print(self.tag2_tag1[0])
+				print(self.tag2_tag1[1])
+				print(self.tag2_tag1[2])
+				
+				print('\n')
+				print('Position of cam wrt tag 1:')
+				print(self.cam_tag_1_pos_x)
+				print(self.cam_tag_1_pos_y)
+				print(self.cam_tag_1_pos_z)
 
-				self.tag_2_tag_1_pos_x = self.cam_tag_1_pos_x - self.cam_tag_2_pos_x
-				self.tag_2_tag_1_pos_y = self.cam_tag_1_pos_y - self.cam_tag_2_pos_y
+				self.cam_pose = PoseStamped()
+				self.cam_pose.header.stamp = rospy.Time.now()
+				self.cam_pose.header.frame_id = "home"
+				self.cam_pose.pose.position.x = self.cam_tag_1_pos_x
+				self.cam_pose.pose.position.y = self.cam_tag_1_pos_y
+				self.cam_pose.pose.position.z = self.cam_tag_1_pos_z
+				self.cam_pose.pose.orientation = self.tag_2_orientation
+				self.cam_wrt_tag_1_pub.publish(self.cam_pose)
 
-				self.cam_tag_1 = PoseStamped()
-				self.cam_tag_1.header.stamp = rospy.Time.now()
-				self.cam_tag_1.header.frame_id = "home"
-				self.cam_tag_1.pose.position.x = self.cam_tag_2_pos_x-self.tag_2_tag_1_pos_x
-				self.cam_tag_1.pose.position.y = self.cam_tag_2_pos_x-self.tag_2_tag_1_pos_y
-				self.cam_wrt_tag_1_pub.publish(self.cam_tag_1)
-			elif self.tag_1_detected_flag == True:
-				print('Publishing using tag 1')
-				self.cam_tag_1 = PoseStamped()
-				self.cam_tag_1.header.stamp = rospy.Time.now()
-				self.cam_tag_1.header.frame_id = "home"
-				self.cam_tag_1.pose.position.x = self.cam_tag_1_pos_x
-				self.cam_tag_1.pose.position.y = self.cam_tag_1_pos_y
-				self.cam_wrt_tag_1_pub.publish(self.cam_tag_1)
 
-			self.rate_tag_detector.sleep()
+			elif self.tag_1_detected_flag==True:
+				print('USING TAG 1:')
+				print('Position of cam wrt tag 1:')
+				print(self.cam_tag_1_pos_x)
+				print(self.cam_tag_1_pos_y)
+				print(self.cam_tag_1_pos_z)
+				print('\n')
+				self.cam_pose = PoseStamped()
+				self.cam_pose.header.stamp = rospy.Time.now()
+				self.cam_pose.header.frame_id = "home"
+				self.cam_pose.pose.position.x = self.cam_tag_1_pos_x
+				self.cam_pose.pose.position.y = self.cam_tag_1_pos_y
+				self.cam_pose.pose.position.z = self.cam_tag_1_pos_z
+				self.cam_pose.pose.orientation = self.tag_1_orientation
+				self.cam_wrt_tag_1_pub.publish(self.cam_pose)
+			self.rate.sleep()
 
 	#Current state subscriber
 	def callback_tag_detection1(self,state):
-		self.cam_tag_1_pos_x = -state.position.x
-		self.cam_tag_1_pos_y = -state.position.y
-		#print('Tag 1 callback')
-		self.tag_1_detected_flag = True
+		self.tag_1_orientation = state.orientation
+		self.tag_1_pos_x = state.position.x
+		self.tag_1_pos_y = state.position.y
+		self.tag_1_pos_z = state.position.z
 		
-	#Current state subscriber
+		
+		self.tag1_cam_quat = Quaternion([self.tag_1_orientation.w, self.tag_1_orientation.x, self.tag_1_orientation.y, self.tag_1_orientation.z])
+
+		self.tag1_cam_matrix = self.tag1_cam_quat.transformation_matrix
+
+		self.tag1_cam_matrix[0][3] = state.position.x
+		self.tag1_cam_matrix[1][3] = state.position.y
+		self.tag1_cam_matrix[2][3] = state.position.z
+
+		self.cam_tag1_matrix = inv(self.tag1_cam_matrix)
+
+		#print(self.cam_tag1_matrix)
+
+		self.cam_tag_1_pos_x = self.cam_tag1_matrix[0][3]
+		self.cam_tag_1_pos_y = self.cam_tag1_matrix[1][3]
+		self.cam_tag_1_pos_z = self.cam_tag1_matrix[2][3]
+		if self.tag_2_detected_flag==True:
+			self.tag2_tag1 = [self.cam_tag_2_pos_x-self.cam_tag_1_pos_x,self.cam_tag_2_pos_y-self.cam_tag_1_pos_y,self.cam_tag_2_pos_z-self.cam_tag_1_pos_z]
+			self.tag_2_diff_calculated_flag=True
+		self.tag_1_detected_flag = True
+
 	def callback_tag_detection2(self,state):
-		self.cam_tag_2_pos_x = -state.position.x
-		self.cam_tag_2_pos_y = -state.position.y
+		self.tag_2_orientation = state.orientation
+		self.tag_2_pos_x = state.position.x
+		self.tag_2_pos_y = state.position.y
+		self.tag_2_pos_z = state.position.z
+		
+		
+		self.tag2_cam_quat = Quaternion([self.tag_2_orientation.w, self.tag_2_orientation.x, self.tag_2_orientation.y, self.tag_2_orientation.z])
+		
+		self.tag2_cam_matrix = self.tag2_cam_quat.transformation_matrix
+
+		self.tag2_cam_matrix[0][3] = state.position.x
+		self.tag2_cam_matrix[1][3] = state.position.y
+		self.tag2_cam_matrix[2][3] = state.position.z
+
+		self.cam_tag2_matrix = inv(self.tag2_cam_matrix)
+
+		#print(self.cam_tag2_matrix)
+
+		self.cam_tag_2_pos_x = self.cam_tag2_matrix[0][3]
+		self.cam_tag_2_pos_y = self.cam_tag2_matrix[1][3]
+		self.cam_tag_2_pos_z = self.cam_tag2_matrix[2][3]
+		if self.tag_2_diff_calculated_flag==True:
+			self.cam_tag_1_pos_x = self.cam_tag_2_pos_x - self.tag2_tag1[0]
+			self.cam_tag_1_pos_y = self.cam_tag_2_pos_y - self.tag2_tag1[1]
+			self.cam_tag_1_pos_z = self.cam_tag_2_pos_z - self.tag2_tag1[2]
+			self.tag_1_pos_calculated_flag=True
+
+			
+
 		self.tag_2_detected_flag = True
-
-	#Current state subscriber
-	def callback_tag_detection3(self,state):
-		self.cam_tag_3_pos_x = -state.position.x
-		self.cam_tag_3_pos_y = -state.position.y
-		self.tag_3_detected_flag = True
-
-	#Current state subscriber
-	def callback_tag_detection4(self,state):
-		self.cam_tag_4_pos_x = -state.position.x
-		self.cam_tag_4_pos_y = -state.position.y
-		self.tag_4_detected_flag = True
 
 def main(args):
 	rospy.init_node('offb_node', anonymous=True)
